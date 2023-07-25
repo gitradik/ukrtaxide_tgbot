eadbd1ada07a1b40f4e5d5c7d85f2dd4395a4b56
gitradik <radik.softdev@gmail.com>
2023-07-25 08:21:48 +0200

    feature confirmation buttons

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
+import asyncio
 from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.utils.exceptions import InvalidQueryID
 
 load_dotenv()
 TOKEN = os.getenv('TG_BOT_TOKEN')
@@ -23,6 +23,7 @@ async def start(message: types.Message) -> None:
     
 # Create a map to store user IDs who have already pressed the button
 users_pressed_button = {}
+users_pressed_confirmation_button = {}
 # Create a set to store user IDs who have already pressed the button
 users_pressed_button_without_username = set()
 
@@ -32,7 +33,8 @@ async def handle_location(message: types.Message) -> None:
         latitude = message.location.latitude
         longitude = message.location.longitude
 
-        users_pressed_button[user.id] = 0
+        users_pressed_button[user.id] = False
+        users_pressed_confirmation_button[user.id] = False
             
         # Save the location in the global dictionary using user_id as the key
         user_locations[user.id] = {'latitude': latitude, 'longitude': longitude}
@@ -41,16 +43,57 @@ async def handle_location(message: types.Message) -> None:
 
 
         await message.reply(
-            f"Привет, {user.mention}! Если у вас есть возможность начать работать, пожалуйста, нажмите на кнопку [Свободен] ниже, чтобы поделиться своей Геолокацией. \n\n Так мы сможем отправить вашу геометку📍в нужную группу, чтобы люди могли воспользоваться вашим такси-сервисом. \n Спасибо за вашу готовность помочь! 🚕🌟",
+            f"Привет, {user.mention}! Если у вас есть возможность начать работать, пожалуйста, нажмите на кнопку [Свободен] ниже, чтобы поделиться своей Геолокацией. \n\n Так мы сможем отправить вашу 📍геометку в нужную группу, чтобы люди могли воспользоваться вашим такси-сервисом. \n Спасибо за вашу готовность помочь! 🚕🌟",
             reply_markup=keyboard_free,
         )
     else:
         await message.reply(f"Простите, {user.mention}, но мы не можем получить доступ к вашему местоположению.\n\n Пожалуйста, свяжитесь с администратором чата @ramal_softdev для помощи. Будем ждать вашего обращения и надеемся, что сможем предоставить вам нашу услугу такси в ближайшее время. \n Спасибо за понимание! 🚕🌟😊")
 
+
+async def confirm_free_btn(query: types.CallbackQuery) -> bool:
+    await query.answer()
+
+    # Create a queue to pass the user's confirmation choice
+    confirmation_queue = asyncio.Queue()
+
+    # Create an InlineKeyboardMarkup with "Да" (Yes) and "Нет" (No) buttons
+    confirm_keyboard = InlineKeyboardMarkup().add(
+        InlineKeyboardButton("Да", callback_data="confirm_yes"),
+        InlineKeyboardButton("Нет", callback_data="confirm_no")
+    )
+
+    await query.message.reply(
+        "Вы уверены, что хотите продолжить?",
+        reply_markup=confirm_keyboard
+    )
+
+    # Define the handler for the user's choice
+    async def handle_confirmation(confirmation_query: types.CallbackQuery):
+        await confirmation_query.answer()
+        if confirmation_query.data == "confirm_yes":
+            await confirmation_queue.put(True)
+        elif confirmation_query.data == "confirm_no":
+            await confirmation_queue.put(False)
+        else:
+            # Handle other actions if needed
+            await confirmation_queue.put(False)
+
+    # Register the handler for the user's choice
+    dp.register_callback_query_handler(handle_confirmation, lambda q: q.message.message_id == query.message.message_id)
+
+    # Wait until the user makes a choice
+    confirmed = await confirmation_queue.get()
+
+    # Unregister the handler after it has been triggered
+    dp.unregister_callback_query_handler(handle_confirmation)
+
+    # Return the user's choice (True for "Да" and False for "Нет")
+    return confirmed
+
 async def free_btn(query: types.CallbackQuery) -> None:
     await query.answer()
     user = query.from_user
-
+    
     # Check if the user not have @username and haven't received the reminder message yet
     if user.username is None:
         if user.id not in users_pressed_button_without_username:
@@ -61,32 +104,52 @@ async def free_btn(query: types.CallbackQuery) -> None:
     elif user.id in users_pressed_button_without_username:
         users_pressed_button_without_username.remove(user.id)
 
-    users_pressed_button[user.id] += 1
-    
     # Check if the user has already pressed the button
-    if users_pressed_button[user.id] > 1:
-        if users_pressed_button[user.id] == 2:
-            await query.message.reply("Вы уже нажали кнопку [Свободен]. Если хотите обновить геометку📍, просто отправьте свою Геолокацию ещё раз.")
+    if users_pressed_button[user.id] is True:
+        await query.message.reply("Вы уже нажали кнопку [Свободен]. Если хотите обновить 📍геометку, просто отправьте свою Геолокацию ещё раз.")
+        return
+    
+    users_pressed_button[user.id] = True
+
+    # Call the confirm_free_btn handler to show the confirmation model window
+    await confirm_free_btn(query)
+
+# Add a new callback query handler to handle the "Да" and "Нет" buttons from the confirmation model window
+async def handle_confirmation(query: types.CallbackQuery) -> None:
+    await query.answer()
+    action = query.data
+    user = query.from_user
+    
+    if users_pressed_confirmation_button[user.id] is True:
         return
 
-    # Get the user's location from the global dictionary using user_id as the key
-    location = user_locations.get(user.id)
+    users_pressed_confirmation_button[user.id] = True
 
-    if location:
-        latitude = location['latitude']
-        longitude = location['longitude']
+    if action == "confirm_yes":
+        # Get the user's location from the global dictionary using user_id as the key
+        location = user_locations.get(user.id)
 
-        await bot.send_message(
-            chat_id=CHAT_ID,
-            text=f"Привет👋! Я ваш таксист @{user.username}, готов помочь вам с комфортной поездкой 🚕🌟.\n\nПожалуйста, отправьте мне свою Геолокацию из меню 📎, и я приеду к вам! С нетерпением жду возможности вам помочь с перемещением по городу.\nСпасибо за выбор нашего такси-сервиса, и до скорой встречи!😊",
-        )
+        if location:
+            latitude = location['latitude']
+            longitude = location['longitude']
 
-        # Send the location map to the group
-        await bot.send_location(chat_id=CHAT_ID, latitude=latitude, longitude=longitude)
+            await bot.send_message(
+                chat_id=CHAT_ID,
+                text=f"Привет👋! Я ваш таксист @{user.username}, готов помочь вам с комфортной поездкой 🚕🌟.\n\nПожалуйста, отправьте мне свою Геолокацию из меню 📎, и я приеду к вам! С нетерпением жду возможности вам помочь с перемещением по городу.\nСпасибо за выбор нашего такси-сервиса, и до скорой встречи!😊",
+            )
 
-        await query.message.reply(f"Благодарим вас за предоставленное местоположение.\n\nМы успешно отправили его в группу {CHAT_ID}. Если вы захотите обновить свою геометку📍в этой группе, просто повторно отправьте свою Геолокацию.")
+            # Send the location map to the group
+            await bot.send_location(chat_id=CHAT_ID, latitude=latitude, longitude=longitude)
+
+            await query.message.reply(f"Благодарим вас за предоставленное местоположение.\n\nМы успешно отправили его в группу {CHAT_ID}. Если вы захотите обновить свою 📍геометку в этой группе, просто повторно отправьте свою Геолокацию.")
+        else:
+            await query.message.reply(f"Простите, {user.mention}, но мы не получили вашего местоположения.\n\nПожалуйста, попробуйте отправить его ещё раз. Если у вас возникнут какие-либо проблемы, вы также можете написать администратору чата @ramal_softdev для помощи.\nМы с нетерпением ждем вашего запроса и готовы предоставить вам отличный сервис! 🚕🌟😊")
+
+    elif action == "confirm_no":
+        await query.message.reply("Вы отменили действие по отправке вышей 📍геометки в группу. Если вы захотите стать доступным для клиентов, просто повторно отправьте свою Геолокацию.")
     else:
-        await query.message.reply(f"Простите, {user.mention}, но мы не получили вашего местоположения.\n\nПожалуйста, попробуйте отправить его ещё раз. Если у вас возникнут какие-либо проблемы, вы также можете написать администратору чата @ramal_softdev для помощи.\nМы с нетерпением ждем вашего запроса и готовы предоставить вам отличный сервис! 🚕🌟😊")
+        # Handle other actions if needed
+        pass
 
 async def on_startup(dp):
     # Set up webhook
@@ -103,18 +166,25 @@ def main():
 
     # Add handler for the "Свободен" button
     dp.register_callback_query_handler(free_btn, text="free")
+    # Add handler for the confirmation of the "Свободен" button
+    dp.register_callback_query_handler(confirm_free_btn, text="confirm_free")
+
+    # Add handler for the "Да" (Yes) and "Нет" (No) buttons from the confirmation model window
+    dp.register_callback_query_handler(handle_confirmation, text="confirm_yes")
+    dp.register_callback_query_handler(handle_confirmation, text="confirm_no")
+
 
     # test localhost
-    # executor.start_polling(dispatcher=dp, skip_updates=True)
+    executor.start_polling(dispatcher=dp, skip_updates=True)
     # Start the webhook
-    executor.start_webhook(
-        dispatcher=dp,
-        webhook_path="/",
-        on_startup=on_startup,
-        skip_updates=True,
-        host=HOST,
-        port=PORT,
-    )
+    # executor.start_webhook(
+    #     dispatcher=dp,
+    #     webhook_path="/",
+    #     on_startup=on_startup,
+    #     skip_updates=True,
+    #     host=HOST,
+    #     port=PORT,
+    # )
 
 if __name__ == "__main__":
     main()
